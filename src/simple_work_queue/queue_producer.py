552b1500be0acc84b9a3d5de6f3df87d43f1ee8b
Jason Luther <jason@ixid.net>
2024-04-07 06:57:08 -0400

    Fix format

--- src/simple_work_queue/queue_producer.py
+++ src/simple_work_queue/queue_producer.py
@@ -1,9 +1,11 @@
-'''
+"""
 QueuePublisher defines a protocol for publishing messages to a queue. It will have a single method, publish, that takes a message as an argument and returns a boolean value indicating whether the message was successfully published. The QueuePublisherSpoolDir and QueuePublisherPubSub classes will implement this protocol for publishing messages to a spool directory and a pub/sub queue, respectively.
-'''
+"""
+
 from typing import Protocol
 import os
 import uuid
+
 
 class QueuePublisher(Protocol):
     def publish(self, message: str) -> bool: ...
@@ -19,7 +21,9 @@ class QueuePublisherSpoolDir(QueuePublisher):
             if not os.path.exists(self.spool_dir):
                 os.makedirs(self.spool_dir)
             request_id = uuid.uuid1()
-            with open(os.path.join(self.spool_dir, f"{request_id}{self.file_extension}"), "w") as f:
+            with open(
+                os.path.join(self.spool_dir, f"{request_id}{self.file_extension}"), "w"
+            ) as f:
                 f.write(message)
             return True
         except Exception as e:
